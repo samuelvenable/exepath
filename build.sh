c6ed9d4e012a1a6c7e7d6c28805971d69cc4f6f1
high on tantor <josephvenable@verizon.net>
2026-04-22 03:54:13 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
-  clang++ exe/exe.cpp main.cpp -o a.out -std=c++17 -Wall -arch arm64 -arch x86_64; ./a.out;
+  clang++ exe-path/exe-path.cpp main.cpp -o a.out -std=c++17 -Wall -arch arm64 -arch x86_64; ./a.out;
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ exe/exe.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+    g++ exe-path/exe-path.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
   else
-    clang++ exe/exe.cpp main.cpp -o a.out -std=c++17 -Wall; ./a.out;
+    clang++ exe-path/exe-path.cpp main.cpp -o a.out -std=c++17 -Wall; ./a.out;
   fi
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ exe/exe.cpp main.cpp -o a.out -std=c++17 -Wall -static; ./a.out;
+  clang++ exe-path/exe-path.cpp main.cpp -o a.out -std=c++17 -Wall -static; ./a.out;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ exe/exe.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+  g++ exe-path/exe-path.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ exe/exe.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+  g++ exe-path/exe-path.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ exe/exe.cpp main.cpp -o a.out -std=c++17 -Wall -lkvm -static; ./a.out;
+  clang++ exe-path/exe-path.cpp main.cpp -o a.out -std=c++17 -Wall -lkvm -static; ./a.out;
 elif [ $(uname) = "SunOS" ]; then
-  g++ exe/exe.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc; ./a.out;
+  g++ exe-path/exe-path.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc; ./a.out;
 else
-  g++ exe/exe.cpp main.cpp -o a.out.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./a.out.exe;
+  g++ exe-path/exe-path.cpp main.cpp -o a.out.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./a.out.exe;
 fi
