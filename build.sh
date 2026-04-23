ad4875fb260b33404a242f8f72555c20c0575173
high on tantor <josephvenable@verizon.net>
2026-04-22 20:48:54 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
-  clang++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -arch arm64 -arch x86_64; ./a.out;
+  clang++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -arch arm64 -arch x86_64; ./a.out;
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+    g++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
   else
-    clang++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall; ./a.out;
+    clang++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall; ./a.out;
   fi
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -lelf -lkvm -lpthread -static; ./a.out;
+  clang++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -lelf -lkvm -lpthread -static; ./a.out;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./a.out;
+  g++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./a.out;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./a.out;
+  g++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./a.out;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -lkvm -lpthread -static; ./a.out;
+  clang++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -lkvm -lpthread -static; ./a.out;
 elif [ $(uname) = "SunOS" ]; then
-  g++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -lkvm -lproc; ./a.out;
+  g++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out -std=c++17 -Wall -static-libgcc -lkvm -lproc; ./a.out;
 else
-  g++ exepath/exepath.cpp process/process.cpp main.cpp -o a.out.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./a.out.exe;
+  g++ pidpath/pidpath.cpp process/process.cpp main.cpp -o a.out.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./a.out.exe;
 fi
