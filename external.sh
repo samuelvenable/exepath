63e6ea9904525b3c4fe62664aabb29ba1ed98dce
high on tantor <josephvenable@verizon.net>
2026-05-02 01:44:47 -0400

    Update external.sh

--- external.sh
+++ external.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
-  clang++ getexecname/external.cpp external.cpp -o getexecname -std=c++17 -Wall -arch arm64 -arch x86_64; ./getexecname;
+  clang++ __getexecname/external.cpp external.cpp -o __getexecname -std=c++17 -Wall -arch arm64 -arch x86_64; ./__getexecname;
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ getexecname/external.cpp external.cpp -o getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./getexecname;
+    g++ __getexecname/external.cpp external.cpp -o __getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./__getexecname;
   else
-    clang++ getexecname/external.cpp external.cpp -o getexecname -std=c++17 -Wall; ./getexecname;
+    clang++ __getexecname/external.cpp external.cpp -o __getexecname -std=c++17 -Wall; ./__getexecname;
   fi
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ getexecname/external.cpp external.cpp -o getexecname -std=c++17 -Wall -static; ./getexecname;
+  clang++ __getexecname/external.cpp external.cpp -o __getexecname -std=c++17 -Wall -static; ./__getexecname;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ getexecname/external.cpp external.cpp -o getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./getexecname;
+  g++ __getexecname/external.cpp external.cpp -o __getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./__getexecname;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ getexecname/external.cpp external.cpp -o getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./getexecname;
+  g++ __getexecname/external.cpp external.cpp -o __getexecname -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./__getexecname;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ getexecname/external.cpp external.cpp -o getexecname -std=c++17 -Wall -lkvm -static; ./getexecname;
+  clang++ __getexecname/external.cpp external.cpp -o __getexecname -std=c++17 -Wall -lkvm -static; ./__getexecname;
 elif [ $(uname) = "SunOS" ]; then
-  g++ getexecname/external.cpp external.cpp -o getexecname -std=c++17 -Wall -static-libgcc -lproc; ./getexecname;
+  g++ __getexecname/external.cpp external.cpp -o __getexecname -std=c++17 -Wall -static-libgcc -lproc; ./__getexecname;
 else
-  g++ getexecname/external.cpp external.cpp -o getexecname.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./getexecname.exe;
+  g++ __getexecname/external.cpp external.cpp -o __getexecname.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./__getexecname.exe;
 fi
